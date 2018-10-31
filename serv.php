8c1fcf0368e7b64c9ab59e6d32ea38fb39df636c
Your Name <you@example.com>
2018-10-31 16:40:05 +0300

    first commit

---
+++ serv.php
@@ -0,0 +1,81 @@
+<?
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+
+
+// Define POST case
+switch (true) {
+  case isset($_POST['add_user']:
+  AddUser();
+    break;
+  case isset($_POST['del_user']:
+  DeleteUser();
+    break;
+  case isset($_POST['get_all_users']:
+  GetAllUsers();
+    break;
+  default:
+    echo "Unknown request"
+    break;
+}
+
+
+// Creating new connection to database
+$connection = new mysqli($servername, $username, $password);
+
+// Сheck if connection was established
+if ($connection->connect_error) {
+    die("Failed to connect to database: " . $connection->connect_error);
+}
+
+//FUnction for adding new user to database
+public function AddUser() {
+  if ($_POST["name"] && $_POST["lastname"] && $_POST["age"]) {
+    $SQL = "INSERT INTO users (firstname, lastname, age, ) VALUES (htmlspecialchars($_POST["name"]),
+                                                                  htmlspecialchars($_POST["lastname"]),
+                                                                  htmlspecialchars($_POST["age"]))";
+
+    if ($connectiom->query($SQL) === TRUE) {
+      echo "User added succesfully.";
+    } else {
+      echo "Error adding user: " . $conn->error;
+    }
+
+  } else {
+    echo "Not all required filled are filled or fit for usage";
+  }
+}
+
+//Function for deleting specific user from database
+public function DeleteUser() {
+  $SQL = "DELETE FROM users WHERE id=".$_POST["id"];
+
+  if ($connectiom->query($SQL) === TRUE) {
+    echo "User deleted succesfully.";
+  } else {
+      echo "Error deleting user: " . $conn->error;
+  }
+}
+
+//Get list of users from the base
+public function GetAllUsers() {
+  $SQL = "SELECT firstname, lastname, age FROM users";
+  $result = $connection->query($SQL);
+
+  $rows = array();
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          $rows[] = $r;
+      }
+      return json_encode($rows);
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+}
+
+?>
